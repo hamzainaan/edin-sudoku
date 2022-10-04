a319ab68710f3ab542c6b9bbe3c105e1da2c4aa9
Hamza Inan <51299406+hamzainaan@users.noreply.github.com>
2022-10-05 00:32:48 +0300

    Update sudoku-solver.cpp

--- sudoku-solver.cpp
+++ sudoku-solver.cpp
@@ -2,13 +2,14 @@
 
 #define DEBUG 55
 
+/*
 std::fstream& lineat(std::fstream& file, unsigned int num){
     file.seekg(std::ios::beg);
     for(int i=0; i < num - 1; ++i){
         file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
     }
     return file;
-}
+}*/
 
 //Ana matris
 int matris[9][9];
